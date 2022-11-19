63ab963c72191612c732def6b679e846c0dd6533
Luciano Sardanha <62066960+sardanhaluciano9690@users.noreply.github.com>
2022-11-19 01:05:47 -0300

    task could be added with the addtask button and the project button

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ const addProjClick = document.querySelector('.addProj');
 const btnTodosClick = document.querySelector('.btnTodos');
 //under here the tasks are going to be added
 const addToElement = document.querySelector('.containerTasks');
+
+//under here the projects are going to be added
+const addToElementProj = document.querySelector('.containerProjs');
+
+
+// variables for when adding a task
 const title = document.createElement('p');
 const description = document.createElement('p');
-
 const dueDate = document.createElement('p');
+
+
 
 
 
@@ -16,30 +23,143 @@ let  todoes = {
     title:       "",
     description: "" ,
     dueDate:     "" , 
+
 }
 
-function addTasks(){
- todoes.title = "mow the lawn"   
- todoes.description = "First by gas"   
- todoes.dueDate = "this Saturday"   
- title.innerText = `Title: ${todoes.title}`;
- title.classList.add('paraTaskAdded');
- addToElement.appendChild(title);
+// create the input space for adding tasks 
 
- description.innerText = `Description: ${todoes.description}`;
- description.classList.add('paraTaskAdded');
- addToElement.appendChild(description);
+function createElementsForTaskInput(){
+    // creating the space to get input from the user; 
+    const createForm = document.createElement('form')
+    createForm.classList.add('form');
+    addToElement.appendChild(createForm);
+    const addToForm = document.querySelector('.form');
 
- dueDate.innerText = `dueDate: ${todoes.dueDate}`;
- dueDate.classList.add('paraTaskAdded');
- addToElement.appendChild(dueDate);
+   
+        function addLabel(label){
+        const titleLabel = document.createElement('label')
+        titleLabel.textContent = `${label} \n`
+        titleLabel.classList.add('paraTaskAdded');
+        addToForm.appendChild(titleLabel);
+       
+        }
+
+             addLabel("Title")
+            const titleInput = document.createElement('input');
+            titleInput.placeholder = "Type task name here..";
+            titleInput.type = "text"
+            titleInput.required = "true"
+            addToForm.appendChild(titleInput);
+
+            addLabel("Description")
+            const descriptionInput = document.createElement('input');
+            descriptionInput.placeholder = "Type description";
+            descriptionInput.type = "text"
+            descriptionInput.required = "true"
+            addToForm.appendChild(descriptionInput);
+
+            addLabel("dueDate")
+            const dueDateInput = document.createElement('input');
+            dueDateInput.type = "date"
+            dueDateInput.required = "true"
+            addToForm.appendChild(dueDateInput);
+
+            
+            const addTaskButton = document.createElement('button');
+            addTaskButton.classList.add('buttonAddTask');
+            addTaskButton.textContent  = "+ Add Task"
+            addToForm.appendChild(addTaskButton);
+
+
+
+
+//         <form>
+//       <label for="name">Name:</label>
+//       <input type="text" id="name" name="name" placeholder="Name"><br/>
+//       <label>Email: <input type="email" name="email" placeholder="email"></label><br/>
+//       <label for="birth">Birth : </label>
+//       <input type="date" id="birth" value="birth"><br/>
+
+//         <input type="submit" value="ClickHere">
+
+//   </form>
+
+
+  
 }
-
-
 let buttonAddTaskEvent = document.querySelector('.btnTodos').addEventListener('click', buttonAddTask);
 
-function buttonAddTask(e){
-    addTasks();
+function buttonAddTask(){
+    createElementsForTaskInput();
 
 }
 
+
+// create the input space for adding projects
+
+
+function createElementsForProjInput(){
+    // creating the space to get input from the user; 
+    const createForm = document.createElement('form')
+    createForm.classList.add('formProj');
+    addToElementProj.appendChild(createForm);
+    const addToForm = document.querySelector('.formProj');
+
+   
+        function addLabel(label){
+        const titleLabel = document.createElement('label')
+        titleLabel.textContent = `${label} \n`
+        titleLabel.classList.add('paraTaskAdded');
+        addToForm.appendChild(titleLabel);
+       
+        }
+
+             addLabel("Title")
+            const titleInput = document.createElement('input');
+            titleInput.placeholder = "Type task name here..";
+            titleInput.type = "text"
+            titleInput.required = "true"
+            addToForm.appendChild(titleInput);
+
+            addLabel("Description")
+            const descriptionInput = document.createElement('input');
+            descriptionInput.placeholder = "Type description";
+            descriptionInput.type = "text"
+            descriptionInput.required = "true"
+            addToForm.appendChild(descriptionInput);
+
+            addLabel("dueDate")
+            const dueDateInput = document.createElement('input');
+            dueDateInput.type = "date"
+            dueDateInput.required = "true"
+            addToForm.appendChild(dueDateInput);
+
+            
+            const addTaskButton = document.createElement('button');
+            addTaskButton.classList.add('buttonAddTask');
+            addTaskButton.textContent  = "+ Add Task"
+            addToForm.appendChild(addTaskButton);
+
+
+
+
+//         <form>
+//       <label for="name">Name:</label>
+//       <input type="text" id="name" name="name" placeholder="Name"><br/>
+//       <label>Email: <input type="email" name="email" placeholder="email"></label><br/>
+//       <label for="birth">Birth : </label>
+//       <input type="date" id="birth" value="birth"><br/>
+
+//         <input type="submit" value="ClickHere">
+
+//   </form>
+
+
+  
+}
+let buttonAddProjEvent = document.querySelector('.addProj').addEventListener('click', buttonAddProject);
+
+function buttonAddProject(){
+    createElementsForProjInput();
+
+}
